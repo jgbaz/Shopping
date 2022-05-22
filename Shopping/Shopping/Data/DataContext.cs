d8c609f469dfacd635adf032647a33787c1c747b
jgbaz <jlgbaz@gmail.com>
2022-05-21 21:55:44 -0500

    Finish Country CRUD

---
+++ Shopping/Shopping/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+
+namespace Shopping.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        // Este se coloca para que no se ingrese dos veces el nombre del Pais(Tabla Countries - Campo Country
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
